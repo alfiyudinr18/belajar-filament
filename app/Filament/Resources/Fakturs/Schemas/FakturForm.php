b21a4b06b00b3dcbd2375ea7d78b7c2215815ea1
alfiyudinr18 <alfiyudin46@gmail.com>
2025-09-01 16:26:03 +0700

    Fix: Label

--- app/Filament/Resources/Fakturs/Schemas/FakturForm.php
+++ app/Filament/Resources/Fakturs/Schemas/FakturForm.php
@@ -7,6 +7,7 @@ use Filament\Schemas\Schema;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Repeater;
+use App\Models\Customer as CustomerModel;
 
 class FakturForm
 {
@@ -17,26 +18,33 @@ class FakturForm
                 TextInput::make("kode_faktur")
                 ->columnSpan(2),
                 DatePicker::make("tanggal_faktur"),
-                TextInput::make("kode_customer"),
                 Select::make("customer_id")
+                    ->reactive()
                     ->relationship('customer', 'nama_customer')
-                    ->columnSpan(2),
+                    ->afterStateUpdated(function ($state, $set) {
+                        $customer = CustomerModel::find($state);
+                        if ($customer) {
+                            $set('kode_customer', $customer->kode_customer);
+                        }
+                    }),
+                TextInput::make("kode_customer")
+                ->columnSpan(2),
                 Repeater::make('detail')
                 ->relationship()
                 ->columnSpan(2)
                 ->schema([
                     Select::make('barang_id')
                     ->relationship('barang', 'nama_barang'),
-                    TextInput::make('diskon')
-                    ->numeric(),
                     TextInput::make('nama_barang'),
                     TextInput::make('harga')
-                    ->numeric(),
-                    TextInput::make('subtotal')
                     ->numeric(),
                     TextInput::make('qty')
                     ->numeric(),
                     TextInput::make('hasil_qty')
+                    ->numeric(),
+                    TextInput::make('diskon')
+                    ->numeric(),
+                    TextInput::make('subtotal')
                     ->numeric(),
                 ]),
                 TextInput::make("ket_faktur")
